556252ef62b9f255ef1deca48e483ce15d94e60d
Fryderyk Górski <fryderykgorski@gmail.com>
2020-09-23 20:51:41 +0200

    Added styling for SummaryPage form

--- src/components/DonateForm/SummaryPage.js
+++ src/components/DonateForm/SummaryPage.js
@@ -14,73 +14,80 @@ const SummaryPage = () => {
     }
 
     return (
-        <>
-            <h3 className="donate-form__header">Podsumowanie Twojej darowizny</h3>
-            <div className="donate-form__typeDetails">
-                <h4 className="donate-form__subheader">Oddajesz:</h4>
-                <ul>
-                    <li className="donate-form__list-item">
-                        <p className="list-item__paragraph">{page2.quantity} worków, {page1.itemType}</p>
-                    </li>
+        <div className="donate-form__stepSummary">
 
-                    <li className="donate-form__list-item">
-                        <p className="list-item__paragraph">W lokalizacji {page3.localization}</p>
-                    </li>
-                </ul>
-            </div>
+            <div className="donate-form__form-wrapper">
+                <div className="container">
+                    <h1 className="donate-form__header">Podsumowanie Twojej darowizny:</h1>
 
-            <div className="donate-form__deliveryDetails">
-                <div className="donate-form__address">
-                    <h4 className="donate-form__subheader">Adres odbioru:</h4>
-                    <ul className="donate-form__list">
-                        <li className="donate-form__list-item">
-                            <p className="list-item__paragraph">Ulica</p>
-                            <p className="list-item__paragraph">{page4.street}</p>
-                        </li>
+                    <div className="donate-form__row">
+                        <div className="donate-form__group">
+                            <h3 className="donate-form__subheader">Oddajesz:</h3>
 
-                        <li className="donate-form__list-item">
-                            <p className="list-item__paragraph">Miasto</p>
-                            <p className="list-item__paragraph">{page4.city}</p>
-                        </li>
+                            <div className="donate-form__item">
+                                <img className="donate-form__icon" src={require("../../assets/Icon-1.svg")} />
+                                <p className="donate-form__donationSpecs">{page2.quantity} worków, {page1.itemType}</p>
+                            </div>
 
-                        <li className="donate-form__list-item">
-                            <p className="list-item__paragraph">Kod pocztowy</p>
-                            <p className="list-item__paragraph">{page4.code}</p>
-                        </li>
+                            <div className="donate-form__item">
+                                <img className="donate-form__icon" src={require("../../assets/Icon-4.svg")} />
+                                <p className="donate-form__donationSpecs">dla lokalizacji: {page3.localization}</p>
+                            </div>
+                        </div>
+                    </div>
 
-                        <li className="donate-form__list-item">
-                            <p className="list-item__paragraph">Numer</p>
-                            <p className="list-item__paragraph">{page4.number}</p>
-                        </li>
-                    </ul>
+                    <div className="donate-form__row">
+                        <div className="donate-form__group">
+                            <h3 className="donate-form__subheader">Adres odbioru:</h3>
+                            <div className="donate-form__item">
+                                <p className="donate-form__deliveryData">Ulica</p>
+                                <p className="donate-form__deliveryData">{page4.street}</p>
+                            </div>
+
+                            <div className="donate-form__item">
+                                <p className="donate-form__deliveryData">Miasto</p>
+                                <p className="donate-form__deliveryData">{page4.city}</p>
+                            </div>
+
+                            <div className="donate-form__item">
+                                <p className="donate-form__deliveryData">Kod pocztowy</p>
+                                <p className="donate-form__deliveryData">{page4.code}</p>
+                            </div>
+
+                            <div className="donate-form__item">
+                                <p className="donate-form__deliveryData">Numer telefonu</p>
+                                <p className="donate-form__deliveryData">{page4.number}</p>
+                            </div>
+
+                        </div>
+
+                        <div className="donate-form__group">
+                            <h3 className="donate-form__subheader">Termin odbioru:</h3>
+
+                            <div className="donate-form__item">
+                                <p className="donate-form__deliveryData">Data</p>
+                                <p className="donate-form__deliveryData">{page4.date}</p>
+                            </div>
+
+                            <div className="donate-form__item">
+                                <p className="donate-form__deliveryData">Godzina</p>
+                                <p className="donate-form__deliveryData">{page4.hour}</p>
+                            </div>
+
+                            <div className="donate-form__item">
+                                <p className="donate-form__deliveryData">Uwagi dla&nbsp;kuriera</p>
+                                <p className="donate-form__deliveryData">{page4.comments}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="donate-form__btn-wrapper">
+                        <button className="donate-form__btn" type="button" onClick={handlePrev}>Wstecz</button>
+                        <button className="donate-form__btn" type="button" onClick={handleAccept}>Potwierdzam</button>
+                    </div>
                 </div>
-
-                <div className="donate-form__date">
-                    <h4 className="donate-form__subheader">Termin odbioru:</h4>
-                    <ul>
-                        <li className="donate-form__list-item">
-                            <p className="list-item__paragraph">Data</p>
-                            <p className="list-item__paragraph">{page4.date}</p>
-                        </li>
-
-                        <li className="donate-form__list-item">
-                            <p className="list-item__paragraph">Godzina</p>
-                            <p className="list-item__paragraph">{page4.hour}</p>
-                        </li>
-
-                        <li className="donate-form__list-item">
-                            <p className="list-item__paragraph">Uwagi dla kuriera</p>
-                            <p className="list-item__paragraph">{page4.comments}</p>
-                        </li>
-                    </ul>
-                </div>
             </div>
-
-            <div>
-                <button className="donate-form__btn" type="button" onClick={handlePrev}>Wstecz</button>
-                <button className="donate-form__btn" type="button" onClick={handleAccept}>Potwierdzam</button>
-            </div>
-        </>
+        </div>
     )
 }
 
