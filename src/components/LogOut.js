eff452332755b26e6aac9ddb008af174a1adfc2f
Fryderyk Górski <fryderykgorski@gmail.com>
2020-09-23 23:38:32 +0200

    Added styling to LogOut component

--- src/components/LogOut.js
+++ src/components/LogOut.js
@@ -1,16 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { AppContext } from '../App';
 
 const LogOut = () => {
     const { firebase } = useContext(AppContext);
 
-    const handleClick = () => {
+    useEffect(() => {
         firebase.auth().signOut();
-    }
+    }, [])
+
     return (
-        <div>
-            <h3>Wylogowanie</h3>
-            <button onClick={handleClick}>Wyloguj się</button>
+        <div className="logout">
+            <div className="container">
+                <div className="logout__statement">
+                    <h1 className="logout__header">Wylogowanie nastąpiło pomyślnie!</h1>
+                    <img className="decoration" src={require('../assets/Decoration.svg')} />
+                    <Link to="/" className="logout__link">Strona główna</Link>
+                </div>
+            </div>
         </div>
     )
 }
