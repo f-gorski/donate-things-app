c6c35035257d18b545f2f4fe05e56b0707208e60
Fryderyk Górski <fryderykgorski@gmail.com>
2020-09-24 23:35:42 +0200

    Added validation to Login and Register forms

--- src/components/LogIn/LogInForm.js
+++ src/components/LogIn/LogInForm.js
@@ -1,65 +1,64 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../../App';
 import { Link, useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 const LogInForm = () => {
-    const { firebase, userForm, setUserForm, error, USER_INITIAL, userAuth, setUserAuth } = useContext(AppContext);
+    const { firebase, userForm, setUserForm, USER_INITIAL, setUserAuth } = useContext(AppContext);
     const history = useHistory()
+    const { register, errors, handleSubmit } = useForm();
 
-    console.log(userForm);
-    const { email, passwordOne } = userForm;
-
-    const handleChange = (e) => {
-        const newState = { [e.target.name]: e.target.value }
-
-        setUserForm(prevState => {
-            return { ...prevState, ...newState }
-        });
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email, passwordOne)
+    const onSubmit = (formInputs) => {
+        const { email, passwordOne } = formInputs;
 
         firebase
             .auth()
             .signInWithEmailAndPassword(email, passwordOne)
             .then(authUser => {
                 setUserAuth({ authUser });
-                console.log(authUser.user.email);
                 setUserForm({ ...USER_INITIAL });
                 history.push('/oddaj-rzeczy');
             })
-            .catch(error => setUserForm({ error: error }));
-    }
+            .catch(error => {
+                let errorMsg = ""
 
-    const isInvalid =
-        passwordOne === '' ||
-        email === '';
+                switch (error.code) {
+                    case 'auth/wrong-password':
+                        errorMsg = "Nieprawidłowe hasło";
+                        break;
+                    case "auth/user-not-found":
+                        errorMsg = "Użytkownik nie istnieje"
+                        break;
+                }
+
+                setUserForm({ error: errorMsg });
+            });
+    }
 
     return (
         <div className="container">
             <div className="login">
                 <h1 className="login-form__header">Zaloguj się</h1>
                 <img src={require('../../assets/Decoration.svg')} className="decoration" />
-                <form onSubmit={handleSubmit} className="login-form">
+                <form onSubmit={handleSubmit(onSubmit)} className="login-form">
                     <div className="login-box">
                         <div>
                             <label className="login-form__label" htmlFor="email">Email</label>
-                            <input className="login-form__input login-form__input--focused" name="email" id="email" onChange={handleChange} value={userForm.email} type="text" placeholder="Email" autocomplete="off" />
+                            <input className="login-form__input login-form__input--focused" name="email" id="email" type="email" placeholder="Email" autocomplete="off" ref={register({ required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ })} />
+                            {errors.email && <p className="login__error-msg">"Nieprawidłowy format adresu email"</p>}
                         </div>
                         <div>
                             <label className="login-form__label" htmlFor="passwordOne">Hasło</label>
-                            <input className="login-form__input login-form__input--focused" name="passwordOne" id="passwordOne" onChange={handleChange} value={userForm.passwordOne} type="password" placeholder="Hasło" autocomplete="off" />
+                            <input className="login-form__input login-form__input--focused" name="passwordOne" id="passwordOne" type="password" placeholder="Hasło" autocomplete="off" ref={register({ required: true })} />
+                            {errors.passwordOne && <p className="login__error-msg">"Podaj hasło"</p>}
                         </div>
+                        {userForm.error && <p className="login__error-msg">{userForm.error}</p>}
                     </div>
 
                     <div className="btn-wrapper">
                         <Link to='/rejestracja' className="login-form__register-link">Zarejestruj się</Link>
-                        <button className="login-form__btn" disabled={isInvalid} type="submit">Zaloguj się</button>
+                        <button className="login-form__btn" type="submit">Zaloguj się</button>
                     </div>
-
-                    {error && <p>{userForm.error.message}</p>}
                 </form>
             </div>
         </div>
--- src/components/Register/RegisterForm.js
+++ src/components/Register/RegisterForm.js
@@ -1,45 +1,41 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../../App';
 import { Link, useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 const RegisterForm = () => {
-    const {firebase, userForm, setUserForm, error, userInitial, userAuth, setUserAuth} = useContext(AppContext);
+    const { firebase, userForm, setUserForm, error, USER_INITIAL, userAuth, setUserAuth } = useContext(AppContext);
     const history = useHistory();
+    const { register, watch, errors, handleSubmit } = useForm();
 
-    console.log(userForm);
-    const {email, passwordOne, passwordTwo} = userForm;
+    const { email, passwordOne, passwordTwo } = userForm;
 
-    const handleChange = (e) => {
-        const newState = {[e.target.name]: e.target.value}
-        
-
-        setUserForm(prevState => {
-            return {...prevState, ...newState}
-        });
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email, passwordOne)
+    const onSubmit = (formInputs) => {
+        const { email, passwordOne } = formInputs;
 
         firebase
-        .auth()
-        .createUserWithEmailAndPassword(email, passwordOne)
-        .then(authUser => {
-            setUserForm({ ...userInitial });
-            setUserAuth({authUser});
-            console.log(authUser.user.email);
-            history.push('/oddaj-rzeczy');
-        })
-        .catch(error => {
-            setUserForm({ error: error});
-        });
-    }
+            .auth()
+            .createUserWithEmailAndPassword(email, passwordOne)
+            .then(authUser => {
+                setUserForm({ ...USER_INITIAL });
+                setUserAuth({ authUser });
+                history.push('/oddaj-rzeczy');
+            })
+            .catch(error => {
+                let errorMsg = ""
 
-    const isInvalid =
-            passwordOne !== passwordTwo ||
-            passwordOne === '' ||
-            email === '';
+                switch (error.code) {
+                    case 'auth/email-already-in-use':
+                        errorMsg = "Użytkownik o tym adresie już istnieje";
+                        break;
+                    case "auth/network-request-failed":
+                        errorMsg = "Wystąpił błąd sieci. Rejestracja nie powiodła się"
+                        break;
+                }
+
+                setUserForm({ error: errorMsg });
+            });
+    }
 
     return (
 
@@ -48,28 +44,34 @@ const RegisterForm = () => {
                 <div className="register">
                     <h1 className="register-form__header">Załóż konto</h1>
                     <img src={require('../../assets/Decoration.svg')} className="decoration" />
-                    <form onSubmit={handleSubmit} className="register-form">
+                    <form onSubmit={handleSubmit(onSubmit)} className="register-form">
                         <div className="register-box">
                             <div>
                                 <label className="register-form__label" htmlFor="email">Email</label>
-                                <input className="register-form__input register-form__input--focused" name="email" id="email" onChange={handleChange} value={userForm.email} type="email" placeholder="Email" autocomplete="off" />
+                                <input className="register-form__input register-form__input--focused" name="email" id="email" type="email" placeholder="Email" autocomplete="off" ref={register({ required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ })} />
+                                {errors.email && <p className="register__error-msg">"Nieprawidłowy adres email"</p>}
                             </div>
                             <div>
                                 <label className="register-form__label" htmlFor="passwordOne">Hasło</label>
-                                <input className="register-form__input register-form__input--focused" name="passwordOne" id="passwordOne" onChange={handleChange} value={userForm.passwordOne} type="password" placeholder="Hasło" autocomplete="off" />
+                                <input className="register-form__input register-form__input--focused" name="passwordOne" id="passwordOne" type="password" placeholder="Hasło" autocomplete="off" ref={register({ required: true, minLength: 6, maxLength: 20 })} />
+                                {errors.passwordOne && <p className="register__error-msg">"Hasło musi mieć między 6 a 20 znaków"</p>}
                             </div>
                             <div>
                                 <label className="register-form__label" htmlFor="passwordTwo">Powtórz hasło</label>
-                                <input className="register-form__input register-form__input--focused" name="passwordTwo" id="passwordTwo" onChange={handleChange} value={userForm.passwordTwo} type="password" placeholder="Hasło" autocomplete="off" />
+                                <input className="register-form__input register-form__input--focused" name="passwordTwo" id="passwordTwo" type="password" placeholder="Hasło" autocomplete="off" ref={register({ required: true, validate: (value) => value === watch('passwordOne') })} />
+                                {errors.passwordTwo && <p className="register__error-msg">"Hasła muszą być zgodne"</p>}
                             </div>
+
+                            {userForm.error && <p className="register__error-msg">{userForm.error}</p>}
+                        </div>
+                        
+
+                        <div className="btn-wrapper">
+                            <Link to='/logowanie' className="register-form__login-link">Zaloguj się</Link>
+                            <button className="register-form__btn" type="submit">Załóż konto</button>
                         </div>
 
-                    <div className="btn-wrapper">
-                        <Link to='/logowanie' className="register-form__login-link">Zaloguj się</Link>
-                        <button className="register-form__btn" disabled={isInvalid} type="submit">Załóż konto</button>
-                    </div>
-
-                    {error && <p>{userForm.error.message}</p>}
+                        {error && <p>{userForm.error}</p>}
                     </form>
                 </div>
             </div>
